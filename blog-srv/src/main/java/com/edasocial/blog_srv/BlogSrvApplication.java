732721aff4c14bd8f3e95d600e37a969108e5243
cingaldi <cingaldi@scmgroup.com>
2020-05-16 14:12:25 +0200

    added templates to services

---
+++ blog-srv/src/main/java/com/edasocial/blog_srv/BlogSrvApplication.java
@@ -0,0 +1,13 @@
+package com.edasocial.blog_srv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSrvApplication.class, args);
+	}
+
+}
---
+++ blog-srv/src/test/java/com/edasocial/blog_srv/BlogSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edasocial.blog_srv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
