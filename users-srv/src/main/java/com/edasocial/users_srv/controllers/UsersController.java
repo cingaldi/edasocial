1f18559e6a24fdd9da0ca4656b36316d234db8f9
cingaldi <cingaldi@scmgroup.com>
2020-05-16 16:09:38 +0200

    [users-srv] wip add createUser endpoint

---
+++ users-srv/src/main/java/com/edasocial/users_srv/controllers/UsersController.java
@@ -0,0 +1,17 @@
+package com.edasocial.users_srv.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UsersController {
+
+    @PostMapping
+    public ResponseEntity<Void> createUser() {
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
---
+++ users-srv/src/test/java/com/edasocial/users_srv/controllers/UsersControllerTest.java
@@ -0,0 +1,30 @@
+package com.edasocial.users_srv.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class UsersControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void given_user_when_post_should_create_it () throws Exception {
+
+        mockMvc.perform(post("/users"))
+                .andExpect(status().isCreated());
+    }
+
+}
