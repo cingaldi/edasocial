fa7116b3b7d0e4d16813a96a3895bf0517296e39
cingaldi <cingaldi@scmgroup.com>
2020-07-11 16:47:42 +0200

    [users-srv] remvoed unnecessary code to make test pass

--- users-srv/src/test/java/com/edasocial/users_srv/controllers/UsersControllerTest.java
+++ users-srv/src/test/java/com/edasocial/users_srv/controllers/UsersControllerTest.java
@@ -2,6 +2,7 @@ package com.edasocial.users_srv.controllers;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
@@ -19,7 +20,7 @@ public class UsersControllerTest {
 
     @Autowired
     MockMvc mockMvc;
-
+    
     @Test
     public void given_user_when_post_should_create_it () throws Exception {
 
