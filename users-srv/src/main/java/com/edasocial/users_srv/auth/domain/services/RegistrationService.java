329fb41875c84f6f47b8f05778b36870270a0f41
cingaldi <carmine.ingaldi@glovoapp.com>
2020-08-12 21:41:36 +0200

    added domain packages and service class for registration use case

---
+++ users-srv/src/main/java/com/edasocial/users_srv/auth/domain/services/RegistrationService.java
@@ -0,0 +1,4 @@
+package com.edasocial.users_srv.auth.domain.services;
+
+public class RegistrationService {
+}
---
+++ users-srv/src/test/java/com/edasocial/users_srv/auth/domain/services/RegistrationServiceTest.java
@@ -0,0 +1,7 @@
+package com.edasocial.users_srv.auth.domain.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceTest {
+
+}
